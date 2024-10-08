15a5d95146185f2c9bf02cd8b08f8311757a53c4
Paul Lian <49074561+vk3002h@users.noreply.github.com>
2024-10-08 15:00:34 +0800

    Fix and Constructors

--- Assets/Scripts/Weapons/WeaponMount.cs
+++ Assets/Scripts/Weapons/WeaponMount.cs
@@ -1,12 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class WeaponMount : MonoBehaviour
 {
-    public float
+    private GameObject _target;
+    private float _leftRotationLimit;
+    private float _rightRotationLimit;
+    private float _rotationSpeed;
+    private Weapon _mountedWeapon;
+    private MechBehavior _mechBehavior;
+
+    private void Update()
+    {
+        
+    }
+
+    private void Aim(GameObject target)
+    {
+        Vector3 targetPosition = new Vector3(0,0,0);
+        if((_mountedWeapon.weaponType == WeaponScriptable.WeaponType.Missile || _mountedWeapon.weaponType == WeaponScriptable.WeaponType.Kenetic) && !_mountedWeapon.homing)
+        {
+            targetPosition = _target.transform.position;
+        }
+        else
+        {
+
+        }
+    }
+
+    public void SetTarget(GameObject target)
+    {
+        _target = target;
+    }
     public WeaponMount(WeaponMountScriptable weaponMountStats, WeaponScriptable scriptable)
     {
-
+        _leftRotationLimit = weaponMountStats.leftRorationLimit;
+        _rightRotationLimit = weaponMountStats.rightRorationLimit;
+        _rotationSpeed = weaponMountStats.rorationSpeed;
+        _mountedWeapon = new Weapon(scriptable, _mechBehavior);
     }
 }
